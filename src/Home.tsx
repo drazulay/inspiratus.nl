32e95b2e325d5141dfa3f313f528c82ace424314
Daniel R. Azulay <daniel@danielazulay.eu>
2024-10-05 00:20:49 +0200

    First working version with video

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,19 @@
 import './styles.css';
 
+import videoSrc from './assets/bg_video.mp4';
+
 const Hpme = () => {
   return (
     <div className="text-container">
-        <video autoPlay={true} muted={true} loop={true} id="bg_video">
-          <source src="/src/assets/bg_video.mp4" type="video/mp4" />
-        </video>
+        <video id="bg_video"
+               src={videoSrc}
+               autoPlay={true}
+               loop={true}
+               mediaGroup="bg"
+               preload="auto"
+               playsInline={true}
+               muted={true} />
+
         <h1>inspir@tus.nl</h1>
       </div>
   )
---
+++ src/NotFound.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+import videoSrc from './assets/bg_video.mp4';
+
+const NotFound = () => {
+  return (
+    <div className="text-container">
+        <h1>404</h1>
+      </div>
+  )
+}
+
+export default NotFound;
